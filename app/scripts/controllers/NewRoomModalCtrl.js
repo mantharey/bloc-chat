565ef0b52ccf1c1a9ecef613d8761f560a220c10
mantharey <Samantha@Macintosh-3.local>
2016-09-21 23:20:27 -0400

    Checkpoint 3: Completed

--- app/scripts/controllers/NewRoomModalCtrl.js
+++ app/scripts/controllers/NewRoomModalCtrl.js
@@ -1,15 +1,10 @@
 (function() {
     function NewRoomModalCtrl(Room, $uibModalInstance) {
         var $ctrl = this;
-        $ctrl.foo = "bar"
+        
           $ctrl.rooms = Room.all
-          //
-          // // Remember to do this to manipulate and debug things:
-          // window.foo = this.rooms
-          //
+
           this.createNewRoom = function(newRoom){
-            console.log("Meow")
-            console.log(newRoom)
             $ctrl.rooms.$add({name: newRoom});
           }
 
